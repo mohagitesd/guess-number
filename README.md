4418e62ab5fef57bcb94f66abeda9d4c0eaa79db
Mohamadou Sy <mohabousy@gmail.com>
2023-11-28 19:23:29 +0100

    HTML3

---
+++ README.md
@@ -0,0 +1,9 @@
+# On écrit en Markdown
+
+- description du projet
+- conseils d'installation 
+- mode d'emploi
+- licence 
+- infos le(s) auteur(s)
+- Historique des Modifications
+- Capture d'écrans
--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
 const inputValue = document.getElementById("inputValue");
 const btnCheck = document.getElementById("btnCheck");
 const textTry = document.getElementById("textTry");
+const textResponse = document.getElementById("textResponse");
+
+function randInt(mini, maxi) {
+    return mini + Math.floor(maxi * Math.random());
+}
+/*for (let i = 1; i < 100; i++) {
+    console.log(randInt(1, 6));
+}*/
+let computerNumber = randInt(1, 100);
+console.log(computerNumber);
+
+let guessTry = 10;
+
+btnCheck.onclick = function () {
+    let userNumber = Number(inputValue.value);
+    if (userNumber == computerNumber) {
+
+
+        console.log("Youpi");
+
+    } else {
+        if (userNumber > computerNumber) {
+            textResponse.innerHTML = "Trop grand";
+        } else {
+            textResponse.innerHTML = "Trop petit"
+        }
+        // retirer un essai
+        /*  guessTry = guessTry-1;
+          guessTry -= 1;*/
+        guessTry--;
+        textTry.innerHTML = "Encore " + guessTry + " essais";
+        if (guessTry <= 0) {
+            textResponse.innerHTML = "Perdu, aucun essai restant";
+            btnCheck.disabled = true;
+        }
+
+
+
+
+
+    }
+}
+
+
